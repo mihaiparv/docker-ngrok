f30774a7ef3099ba530a6fd024c388c99715fd7d
Mihai Parv <mihai.parv@iquestgroup.com>
2015-08-31 19:58:42 +0300

    Corrected arguments concatenation and removed help as it is no longer needed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,10 @@ if [ "$1" = "/bin/sh" ]; then
   shift
 fi
 
-ARGS="start --none"
+ARGS="start --none "
 
 if [ -n "$NGROK_AUTH" ]; then
-  ARGS="-authtoken=$NGROK_AUTH "
+  ARGS="$ARGS -authtoken $NGROK_AUTH "
 fi
-
-case "$1" in
-  -h|help)  ARGS=$1 ;;
-  *)        ARGS="$ARGS -log stdout $* $FWD" ;;
-esac
 
 exec /bin/ngrok $ARGS
